810666ab41d8eb6b8de37b1fe432e91f5fee91a0
KatieAJenkins <kind2karma@gmail.com>
2017-02-28 07:33:00 -0700

    organized pseudo code cleaned up

--- swaps.js
+++ swaps.js
@@ -10,12 +10,9 @@ var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 function swap (array, index_A, index_B) {
   //temporarily store the contents of the first index
   var temp = array[index_A];
-  // console.log(temp);
 
   //assign the contents of the second index to the first index
   array[index_A] = array[index_B];
-  // console.log(array[index_A]);
-  // console.log(array[index_B]);
 
   //take temporarily stored contents and assign to second index
   array[index_B] = temp;
@@ -24,20 +21,29 @@ function swap (array, index_A, index_B) {
 swap(array, 5 , 7);
 // console.log(array);
 
+/////////REVERSE FUNCTION//////////
+
 // Implement a reverse function for arrays using your swap function.
 
 // Write pseudo-code to solve this, then write javascript.
 
-//REVERSE FUNCTION
+  //create variable for starting value in array
+  //create variable for last value in array (array.length - 1)
+  //while starting value is less than last value
+  //assign starting value to temp
+  //replace starting value with last value
+  //assign last value to temp/first value
+  //count up 1 from starting value
+  //count down 1 from ending value
+
   //Inputs: array [1, 2, 3, 4]
   //Outputs: array [4, 3, 2, 1] (existing)
 
-var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var array = [1, 2, 3, 4];
 
 function reverse(array){
-  //starting value in array
+
   var i = 0;
-  //find last value in array (last index = array.length -1)
   var j = array.length - 1;
 
   while (i < j){
@@ -45,12 +51,8 @@ function reverse(array){
     array[i] = array[j];
     array[j] = temp;
 
-    //count up through i
     i++;
-    // console.log('i', array[i]);
-    //count down through j
     j--;
-    // console.log(array[j]);
   }
 }
 
